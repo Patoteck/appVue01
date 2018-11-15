d7aab8b327ca10f426094cbb649ec58b14602163
Patrcio Hernandez <patoteckcomputacion@gmail.com>
2018-11-15 02:13:05 -0300

    addproject

---
+++ precache-manifest.f29a61f35fea229bfa59b427bcb16c94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "859eacc480f25650a49f",
+    "url": "/js/chunk-vendors.272e5479.js"
+  },
+  {
+    "revision": "af02fd5ec303ea7e6972",
+    "url": "/js/app.759b4522.js"
+  },
+  {
+    "revision": "e6f8897ae0ef8e5387e8",
+    "url": "/js/about.0bf3fb79.js"
+  },
+  {
+    "revision": "2f91980a36c857ce59ad47bacb290063",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "af02fd5ec303ea7e6972",
+    "url": "/css/app.dde1df84.css"
+  }
+];
