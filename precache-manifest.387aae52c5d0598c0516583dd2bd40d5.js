33d4ae23175d909b8fb650993fab3fc7b5e8be82
Riolfo, María Fernanda <mfernandariolfo@gmail.com>
2018-11-26 20:11:06 -0300

    app

---
+++ precache-manifest.387aae52c5d0598c0516583dd2bd40d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "859eacc480f25650a49f",
+    "url": "/js/chunk-vendors.272e5479.js"
+  },
+  {
+    "revision": "c8e1a1ba4e1cb8e8da21",
+    "url": "/js/app.4b2345b6.js"
+  },
+  {
+    "revision": "e6f8897ae0ef8e5387e8",
+    "url": "/js/about.0bf3fb79.js"
+  },
+  {
+    "revision": "e8409eab62518c3bb00b9e06af1512e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c8e1a1ba4e1cb8e8da21",
+    "url": "/css/app.dde1df84.css"
+  }
+];
